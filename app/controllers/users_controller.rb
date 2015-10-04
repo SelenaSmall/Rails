89b4fb27385b7e834de1cb8bf222dd8246032726
SelenaSmall <selenawiththetattoo@gmail.com>
2015-10-04 22:38:47 +1300

    added devise views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,60 +1,38 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  after_action :verify_authorized
 
   def index
     @users = User.all
+    authorize User
   end
 
   def show
-  end
-
-  def new
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to [@user, @users], notice: 'user was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.find(params[:id])
+    authorize @user
   end
 
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to [@user], notice: 'user was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+      @user = User.find(params[:id])
+    authorize @user
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
     end
   end
 
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_path(@user), notice: 'user was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    user = User.find(params[:id])
+    authorize user
+    user.destroy
+    redirect_to users_path, :notice => "User deleted."
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
+    def secure_params
+    params.require(:user).permit(:role)
+  end
 
-    def user_params
-      params.require(:user).permit(:user_name, :password, :first_name, :last_name, :email, :phone, :facebook)
-    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150922121741) do
+ActiveRecord::Schema.define(version: 20151003001150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -149,11 +149,24 @@ ActiveRecord::Schema.define(version: 20150922121741) do
   add_index "tables", ["venue_id"], name: "index_tables_on_venue_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "user_name",  limit: 100, null: false
-    t.string   "password",   limit: 100, null: false
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "user_name"
+    t.string   "password"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "venues", force: :cascade do |t|
     t.integer  "organisation_id"
