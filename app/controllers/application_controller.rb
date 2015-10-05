debb5f94a1bd29d09c2308dadfcffe625a41bdfe
SelenaSmall <selenawiththetattoo@gmail.com>
2015-10-06 00:28:21 +1300

    different sign in paths for user and admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
  def after_sign_in_path_for(user)
-    request.env['omniauth.origin'] || stored_location_for(user) || organisations_path
+ 	if current_user.admin?
+ 		request.env['omniauth.origin'] || stored_location_for(user) || organisations_path
+ 	else
+ 		request.env['omniauth.origin'] || stored_location_for(user) || root_path
+ 	end
  end
 
 #  before_action :authenticate_user!
